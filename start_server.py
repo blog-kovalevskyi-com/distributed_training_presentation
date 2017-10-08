5fbd61c5cbfaecd1270926e1e81b8f4da8922718
Viacheslav Kovalevskyi <vsk@amazon.com>
2017-10-08 14:19:16 -0700

    new start server and scheduler

--- start_server.py
+++ start_server.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ.update({
-  "DMLC_ROLE": "scheduler", # Could be "scheduler", "worker" or "server"
+  "DMLC_ROLE": "server", # Could be "scheduler", "worker" or "server"
   "DMLC_PS_ROOT_URI": "127.0.0.1", # IP address of a scheduler
   "DMLC_PS_ROOT_PORT": "9000", # Port of a scheduler
   "DMLC_NUM_SERVER": "1", # Number of servers in cluster
