e48da4deea1b99f5fb0d10edd34ff8274047239a
Viacheslav Kovalevskyi <vsk@amazon.com>
2017-10-09 17:00:33 -0700

    final versin

--- multi_worker_cluster/start_worker_1.py
+++ multi_worker_cluster/start_worker_1.py
@@ -5,7 +5,7 @@ os.environ.update({
   "DMLC_PS_ROOT_URI": "127.0.0.1",
   "DMLC_PS_ROOT_PORT": "9000",
   "DMLC_NUM_SERVER": "1",
-  "DMLC_NUM_WORKER": "1",
+  "DMLC_NUM_WORKER": "2",
   "PS_VERBOSE": "2"
 })
 
